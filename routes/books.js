230a75672ba1850ebcea0748a6b3390a182c0cba
study <susung462@gmail.com>
2023-09-09 19:08:30 +0900

    week2

--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* 도서검색 */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('index', { title: '도서검색', pageName:'books/search.ejs' });
 });
 
 module.exports = router;
