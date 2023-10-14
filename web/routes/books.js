704d9fc87ed110d91e6e27a4ed9001b0296b8f07
seolhwayam <122880487+seolhwayam@users.noreply.github.com>
2023-10-14 18:16:43 +0900

    week07
    week07

--- web/routes/books.js
+++ web/routes/books.js
@@ -9,6 +9,11 @@ router.get('/', function(req, res, next) {
   
 });
 
+//장바구니 페이지 이동
+router.get('/cart', function(req,res){
+  res.render('index',{title:'장바구니',pageName:'books/cart.ejs'});
+})
+
 //렌더링 : ejs 파일(view파일)
 
 module.exports = router;
