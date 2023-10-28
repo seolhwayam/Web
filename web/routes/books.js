eb1c0e4d89b5e68084fbeb498f2db868dc76900a
seolhwayam <122880487+seolhwayam@users.noreply.github.com>
2023-10-28 17:54:48 +0900

    week9
    week9

--- web/routes/books.js
+++ web/routes/books.js
@@ -4,13 +4,13 @@ var router = express.Router();
 //라우터의 역할 : 패스가 ~~일때 ~~해주겠다!
 
 /* 도서검색 */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) { 
   res.render('index', { title: '도서검색', pageName:'books/search.ejs' });
   
 });
 
 //장바구니 페이지 이동
-router.get('/cart', function(req,res){
+router.get('/cart', function(req,res){ 
   res.render('index',{title:'장바구니',pageName:'books/cart.ejs'});
 })
 
--- web/routes/locals.js
+++ web/routes/locals.js
@@ -6,6 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: '지역검색', pageName:'locals/search.ejs' });
 });
 
+//즐겨찾기
+router.get('/favorite', function(req, res, next) {
+  res.render('index', { title: '즐겨찾기', pageName:'locals/favorite.ejs' });
+});
+
 //렌더링 :ejs파일
 
 module.exports = router;
