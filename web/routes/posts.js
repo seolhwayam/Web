d75a6381a078f3bfc2542e4a06dea91e4701f2a2
seolhwayam <122880487+seolhwayam@users.noreply.github.com>
2023-11-18 18:16:01 +0900

    week12
    week12

--- web/routes/posts.js
+++ web/routes/posts.js
@@ -8,4 +8,9 @@ router.get('/', function(req, res, next) {
 router.get('/insert', function(req, res, next) {
     res.render('index', { title: '글쓰기', pageName:'posts/insert.ejs' });
   });
+
+  router.get('/read', function(req, res, next) {
+    const id=req.query.id;
+    res.render('index', { title: '게시글정보', pageName:'posts/read.ejs',id });
+  });
 module.exports = router;
